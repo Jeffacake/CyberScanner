291c118f5c0f23941bee450bc9a879d897abd378
Jeff Jones <jeffbjones777@gmail.com>
2023-11-05 13:11:22 +0000

    Create demo graphs with data
    create and populate data.py file with relevant data and display on tkinter barchart, piechart and linegraph

--- cyberscraper.py
+++ cyberscraper.py
@@ -6,21 +6,58 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 #import numpy (used in conjunction with matplotlib for visualization)
 import numpy as np
+#import dummy data
+from dummyData import attack_frequency, attack_severity, attack_success_rate, cyber_attacks
+
+#rcParams stands for runtime configuration parameters
+plt.rcParams["axes.prop_cycle"] = plt.cycler(
+    color=["#0047AB", "#6E81D6", "#2E5AAC", "#17325E", "#8CB8E8"]
+)
 
 
-def plot():
-    x = np.random.randint(0,10,10)
-    ax.plot(x)
-    canvas.draw()
+    # def plot():
+    #     x = np.random.randint(0,10,10)
+    #     ax.plot(x)
+    #     canvas.draw()
 
-def clear():
-    ax.clear()
-    canvas.draw()
+    # def clear():
+    #     ax.clear()
+    #     canvas.draw()
+
+
+
+#chart1
+fig1, ax1 = plt.subplots() 
+ax1.bar(attack_frequency.keys(), attack_frequency.values())
+ax1.set_title("Attack Frequency")
+ax1.set_xlabel("Attack Type")
+ax1.set_ylabel("Frequency")
+
+
+#chart2
+fig2, ax2 = plt.subplots()
+
+ax2.barh(list(attack_severity.keys()), attack_severity.values()) #barh must take list object
+ax2.set_title("Attack Severity")
+ax2.set_xlabel("Attack Type")
+ax2.set_ylabel("Severity")
+
+
+#chart3
+fig3, ax3, = plt.subplots()
+ax3.pie(attack_success_rate.values(), labels=attack_success_rate.keys())
+ax3.set_title("Attack Success Rate")
+
+fig4, ax4, = plt.subplots()
+ax4.plot(cyber_attacks.keys(), cyber_attacks.values())
+ax4.set_title("Cyber Attacks over 6yr period")
+
+
 
 
 #Initialise Tkinter
 root = tk.Tk()
-root.configure(bg="grey")
+root.config(bg="grey")
 
 #welcome label
 bold_font = font.Font(family="Helvetica", size=12, weight="bold")
@@ -33,21 +70,25 @@ greeting = tk.Label(root, text="Welcome to CyberScraper",
                     )
 greeting.pack(fill=tk.X)
 
-#create frame for graph to fill
-graph_frame = tk.Frame(root, bg="white")  
-graph_frame.pack(fill=tk.BOTH)
+upper_frame = tk.Frame(root)
+upper_frame.pack(fill="both", expand=True)
+canvas1 = FigureCanvasTkAgg(fig1, upper_frame)
+canvas1.get_tk_widget().pack(side="left", fill="both",expand=True)
+canvas1.draw()
 
-#create figure for canvas
-figure, ax = plt.subplots()
-#canvas takes figure and 'frame' parameter)
-canvas = FigureCanvasTkAgg(figure, master=graph_frame)
-#to integrate in tkinter, get the widget for it first
-canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+canvas2 = FigureCanvasTkAgg(fig2, upper_frame)
+canvas2.get_tk_widget().pack(side="left", fill="both", expand="true")
+canvas2.draw()
 
+lower_frame = tk.Frame(root)
+lower_frame.pack(fill="both",expand="true")
 
-plot_graph_button = tk.Button(graph_frame, text="Plot Graph",padx=10,pady=10, command=plot).pack()
+canvas3 = FigureCanvasTkAgg(fig3, lower_frame)
+canvas3.get_tk_widget().pack(side="left",fill="both",expand="true")
+canvas3.draw()
 
-refresh_button = tk.Button(graph_frame, text="Refresh", padx=10,pady=10, command=clear).pack()
+canvas4 = FigureCanvasTkAgg(fig4, lower_frame)
+canvas4.get_tk_widget().pack(side="left", fill="both",expand="true")
 
 root.mainloop()
 #window.update()
---
+++ dummyData.py
@@ -0,0 +1,34 @@
+
+# Dummy data for cyber attacks
+attack_frequency = {
+    "Malware": 350,
+    "Phishing": 500,
+    "DDoS": 200,
+    "Ransomware": 150,
+    "SQL Injection": 300
+}
+
+attack_severity = {
+    "Malware": 3,
+    "Phishing": 2,
+    "DDoS": 1,
+    "Ransomware": 3,
+    "SQL Injection": 2
+}
+
+attack_success_rate = {
+    "Malware": 0.65,
+    "Phishing": 0.45,
+    "DDoS": 0.3,
+    "Ransomware": 0.75,
+    "SQL Injection": 0.5
+}
+
+cyber_attacks = {
+    "2018": 1500,
+    "2019": 1800,
+    "2020": 2200,
+    "2021": 2500,
+    "2022": 3000,
+    "2023": 3200
+}
